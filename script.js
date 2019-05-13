1327cf2b7360fd2410aadc91da07212e4fe86460
bungiemuffins <kkimball1324@gmail.com>
2019-05-13 00:53:41 -0600

    added more javascript **need to debug**

--- script.js
+++ script.js
@@ -12,9 +12,45 @@ window.onload = function() {
             const response = await fetch(url);
             //console.log("response:", response);
             const json = await response.json();
-            //console.log("json:",json)
+            console.log("json:",json)
+
 
             let results = "";
+            //try to change background pic.
+            var today = new Date();
+            var time = today.getHours;
+            var night = false;
+            var weatherDescriptor = json.weather[0].main;
+            var weatherImageURL = "";
+            if(time >= 21 && time <= 5) {
+                night = true;
+            }
+            switch(weatherDescriptor) {
+                case 'Clear':
+                    weatherImageURL += 'clearSky';
+                    break;
+                case 'Snow':
+                    weatherImageURL += 'snow';
+                    break;
+                case 'Clouds':
+                    weatherImageURL += 'cloudy';
+                    break;
+                case 'Rain':
+                    weatherImageURL += 'rainy';
+                    break;
+                default:
+                    weatherImageURL += 'clearSky';
+                    break;
+            }
+            if(night = true){
+                weatherImageURL += 'Night';
+            }
+            else{
+                weatherImageURl += 'Day';
+            }
+            results += '<div class="weatherContainer" style="background-image: url(images/' + weatherImageURL + '.jpg)">';
+
+
             results += '<h2>Weather in ' + json.name + "</h2>";
             for (let i=0; i < json.weather.length; i++) {
                 results += '<img src="http://openweathermap.org/img/w/' + json.weather[i].icon + '.png"/>';
@@ -22,11 +58,14 @@ window.onload = function() {
             results += '<h2>' + json.main.temp + " &deg;F</h2>"
             results += "<p>"
             for (let i=0; i < json.weather.length; i++) {
-                results += json.weather[i].description
+                results += json.weather[i].description;
                 if (i !== json.weather.length - 1)
-                    results += ", "
+                    results += ", ";
             }
             results += "</p>";
+
+            results += "</div>";
+
             document.getElementById("weatherResults").innerHTML = results;
 
         }
@@ -39,12 +78,53 @@ window.onload = function() {
         try{
             const response = await fetch(url2);
             const json = await response.json();
+            console.log("json: ", json);
 
             let forecast = "";
+            //changing background pic of each forecast;
             for (let i = 0; i <json.list.length; i++){
+                //looping to get the background pic for each forecast
+                var night = false;
+                var weatherImageURL = "";
+                var date = json.list[i].dt_txt;
+                var time = '';
+                time += date.charAt(11);
+                time += date.charAt(12);
+                var weatherDescriptor = json.list[i].weather.main;
+                if(time >= 21 && time <= 5) {
+                    night = true;
+                }
+                switch(weatherDescriptor) {
+                    case 'Clear':
+                        weatherImageURL += 'clearSky';
+                        break;
+                    case 'Snow':
+                        weatherImageURL += 'snow';
+                        break;
+                    case 'Clouds':
+                        weatherImageURL += 'cloudy';
+                        break;
+                    case 'Rain':
+                        weatherImageURL += 'rainy';
+                        break;
+                    default:
+                        weatherImageURL += 'clearSky';
+                        break;
+                }
+                if(night = true){
+                    weatherImageURL += 'Night';
+                }
+                else{
+                    weatherImageURl += 'Day';
+                }
+    
+                forecast += '<div class="weatherContainer" style="background-image: url(images/' + weatherImageURL + '.jpg)">';
+                //forecast content
                 forecast += "<h2>" + moment(json.list[i].dt_txt).format('MMMM Do YYYY, h:mm:ss a') + "</h2>";
                 forecast += "<p>Temperature: " + json.list[i].main.temp + "</p>";
                 forecast += '<img src="http://openweathermap.org/img/w/' + json.list[i].weather[0].icon + '.png"/>'
+
+                forecast += "</div>";
             }
             document.getElementById("forecastResults").innerHTML = forecast;
         }
